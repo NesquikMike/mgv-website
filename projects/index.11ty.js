a2910a264b4a6d9d5c915f3fb242ee793d3f9d42
NesquikMike <mgomesvieira13@gmail.com>
2021-08-08 14:07:06 +0100

    starts work on projects section

---
+++ projects/index.11ty.js
@@ -0,0 +1,22 @@
+exports.data = {
+    title: "Projects",
+    layout: "base-layout.11ty.js"
+};
+
+exports.render = function(data) {
+//     return `
+// <br>
+// <ul>
+//   ${data.collections.projects.map(project =>
+//     `<li>
+//       <a href="${project.url}">${project.data.title}</a>
+//       <p>Preview</p>
+//       <p>${project.date}</p>
+//     </li>`
+//     ).join("\n")}
+// </ul>
+//     `;
+    return `
+<p>Poop</p>
+`
+};
