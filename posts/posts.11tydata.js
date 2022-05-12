26439bc42cc4617d124a051831aaa0fa03f54aff
NesquikMike <mgomesvieira13@gmail.com>
2022-05-12 20:14:44 +0100

    adds functionality for draft posts

---
+++ posts/posts.11tydata.js
@@ -0,0 +1,38 @@
+// Creates draft functionality in 11ty
+// More info can be found here: 
+// https://jkc.codes/blog/creating-drafts-in-eleventy/
+require('dotenv').config();
+
+const isDevEnv = process.env.ELEVENTY_ENV === 'development';
+const todaysDate = new Date();
+
+function showDraft(data) {
+	const isDraft = 'draft' in data && data.draft !== false;
+	const isFutureDate = data.page.date > todaysDate;
+	return isDevEnv || (!isDraft && !isFutureDate);
+}
+
+module.exports = function() {
+	return {
+		eleventyComputed: {
+			eleventyExcludeFromCollections: function(data) {
+				if(showDraft(data)) {
+					return data.eleventyExcludeFromCollections;
+				}
+				else {
+					return true;
+				}
+			},
+			permalink: function(data) {
+				if(showDraft(data)) {
+					return data.permalink
+				}
+				else {
+					return false;
+				}
+			}
+		},
+    layout: "base-layout.11ty.js",
+    tags: ["posts"]
+	}
+}
