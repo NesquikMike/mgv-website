4470ebdbeb81a9fc5b0689bc0babf587ff6c2bf4
NesquikMike <mgomesvieira13@gmail.com>
2022-07-15 16:26:09 +0100

    adds 404

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
+// Adds dev functionality for derving 404 pages. More info can be found at this link:
+// https://www.11ty.dev/docs/quicktips/not-found/
+const fs = require("fs");
+const NOT_FOUND_PATH = "_site/404.html";
+
 // We do this so that we can have more control of the HTML output of our markdown
 // Link: https://giuliachiola.dev/posts/add-html-classes-to-11ty-markdown-content/
 const markdownIt = require('markdown-it')
@@ -27,6 +32,28 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
 
     eleventyConfig.setUseGitIgnore(false);
+
+    // To serve 404 page in dev. Code taken from here:
+    // https://www.11ty.dev/docs/quicktips/not-found/ 
+    eleventyConfig.setBrowserSyncConfig({
+      callbacks: {
+        ready: function(err, bs) {
+  
+          bs.addMiddleware("*", (req, res) => {
+            if (!fs.existsSync(NOT_FOUND_PATH)) {
+              throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+            }
+  
+            const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+            // Add 404 http status code in request header.
+            res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+            // Provides the 404 content without redirect.
+            res.write(content_404);
+            res.end();
+          });
+        }
+      }
+    }); 
 }
 
 // This is used to demark the excerpts of the blog post that are used to
---
+++ 404.11ty.js
@@ -0,0 +1,15 @@
+exports.data = {
+    title: "Ooops! You're on the 404 Page 😽",
+    permalink: '404.html',
+    layout: "base-layout.11ty.js",
+    eleventyExcludeFromCollections: true
+};
+
+exports.render = function(data) {
+    return `
+      <div class='four04'>
+        <p>You can find your way back to my homepage <a href='/'>here</a> 👈</p>
+        <p>Or if you think this is a 🪲 you can <a href='https://github.com/NesquikMike/mgv-website/issues'>log an issue here!</a></p>
+      <div>
+    `;
+};
