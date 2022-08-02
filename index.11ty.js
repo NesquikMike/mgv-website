8e8a99b77e8ea23339740d4a42df06a731d9c101
NesquikMike <mgomesvieira13@gmail.com>
2022-08-02 14:08:34 +0100

    tries get rid of reverse

--- index.11ty.js
+++ index.11ty.js
@@ -5,7 +5,7 @@ exports.data = {
 exports.render = function(data) {
     return `
     <ul id='blog-posts'>
-      ${data.collections.posts.reverse().map(blog =>
+      ${data.collections.posts.map(blog =>
         `<li>
           <time>${blog.date.toLocaleDateString('en-UK', data.myProject.dateStringOptions)}</time>
           <a href="${blog.url}"><h2>${blog.data.title}</h2></a>
